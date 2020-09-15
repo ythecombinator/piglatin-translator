ab1cd14c3ed9403af3d1a4ff67d5e14fdcb4611a
Matheus Albuquerque <land@ythecombinator.space>
2020-09-15 21:37:09 +0200

    feat(utils/translator): bootstrap initial checks
    This includes:
    
    - Words that end in “way” are not modified
    - Hyphens are treated as two words

--- src/utils/translator/translator.ts
+++ src/utils/translator/translator.ts
@@ -1,2 +1,40 @@
+import {
+  always,
+  cond,
+  identity,
+  isEmpty,
+  join,
+  map,
+  pipe,
+  split,
+  T as defaultsTo,
+  test,
+  trim,
+} from 'ramda';
+
+import {stringToArray} from 'utils/string';
+
+const keywordRegex = /way$/;
+
+const applyTransformers = (original: string) => {
+  // Some transformers might need to be curried to take the original string as a param
+
+  const result = cond([
+    // We should not apply any transformation on empty strings.
+    [isEmpty, always("")],
+    // RULE: Words that end in “way” are not modified.
+    // Thus we should apply an identity function to it.
+    // This does not go to `.transformers` because it makes no changes to the data.
+    [test(keywordRegex), identity],
+    // All transformers should be composed here:
+    [defaultsTo, pipe(() => {})],
+  ])(original);
+
+  return result;
+};
+
+// RULE: Hyphens are treated as two words
+const transform = pipe(split("-"), map(applyTransformers), join("-"));
+
 // API
-export const translate = (word: string) => "";
+export const translate = pipe(trim, stringToArray, map(transform), join(" "));
